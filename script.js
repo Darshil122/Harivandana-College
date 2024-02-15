6de1a4f23c7ead9c10f4c29c23ee336933e5afea
Darshil122 <darshildabhi2005@gmail.com>
2024-02-15 20:08:30 +0530

    odified

--- script.js
+++ script.js
@@ -5,7 +5,35 @@ function myFunction() {
       x.className += " responsive";
     } else {
       x.className = "topnav";
-    }
+    }//navbar
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+//Courses
+const undergraduateButton = document.getElementById('undergraduateButton');
+const postgraduateButton = document.getElementById('postgraduateButton');
+const undergraduateSection = document.getElementById('undergraduateSection');
+const postgraduateSection = document.getElementById('postgraduateSection');
+
+undergraduateButton.addEventListener('click', () => {
+    undergraduateSection.style.display = 'block';
+    undergraduateSection.style.display = 'flex';
+    postgraduateSection.style.display = 'none';
+});
+
+postgraduateButton.addEventListener('click', () => {
+    undergraduateSection.style.display = 'none';
+    postgraduateSection.style.display = 'block';
+    postgraduateSection.style.display = 'flex';
+    postgraduateSection.classList.add(graduate);
+    postgraduateSection.style.marginLeft = '300px';
+
+});
   }
   //Courses
   const undergraduateButton = document.getElementById('undergraduateButton');
